a62b018e97f815f88dfccda828d815d9dbe3c1f1
Ajax() <ylxdzsw@gmail.com>
2014-12-07 13:49:24 +0800

    add cost function(untested)

--- KERNEL/main.cu
+++ KERNEL/main.cu
@@ -50,10 +50,9 @@ __device__ float _mul(M M1,M M2,int threadid){
 	return a;
 }
 //参数a2是虚拟化之后的，a3是正常的
-__global__ void nnCostFunction(M theta1,M theta2,M X,int * Y,float lambda,M a2,M a3,int num) {
+__global__ void gradient(M theta1,M theta2,M X,int * Y,float lambda,M a2,M a3) {
 	int gid = blockIdx.x*blockDim.x+threadIdx.x;
 	M a1 = X;a1.col--;a1.flag=-1;
-for(int iter=0;iter<num;iter++){
 	if(gid<(a2.col-1)*a2.row){
 		a2.ptr[gid] = sigmoid(mul_(a1,theta1,gid));
 	}
@@ -85,8 +84,6 @@ for(int iter=0;iter<num;iter++){
 	a2.flag=-1;a2.col++;
 }
 
-}
-
 //this function will ruin all of a2,X and P.
 //a2 should be a virtualized matrix!
 //results was placed in the first 5000 elements in a2
@@ -97,10 +94,12 @@ __global__ void predict(M Theta1, M Theta2, M X, M a2, M P){
 	if(gid<5000*25){
 		a2.ptr[gid] = sigmoid(mul_(a1,Theta1,gid));
 	}
+	__syncthreads();
 	if(gid<5000*10){
 		//如果sigmoid函数单调，那这里进不进行这个运算应该无所谓 -ylxdzsw 2014.12.6
 		P.ptr[gid] = sigmoid(mul_(a2,Theta2,gid));
 	}
+	__syncthreads();
 	if(gid < 5000){
 		float temp;
 		float max = -1.0f;
@@ -111,6 +110,50 @@ __global__ void predict(M Theta1, M Theta2, M X, M a2, M P){
 			}
 		}
 		a2.ptr[gid] = max;
+	}
+
+}
+
+//这里使用map reduce大法可以加快速度，但是需要一块与a3+theta1+theta2同大的内存，复制这块内存所花的时间或许不比节约的时间少 -ylxdzsw 2014.12.7
+//tempmemory至少要有两个float大
+
+__device__ int lock = 0;
+
+__global__ void costfunction(M a3, M Theta1, M Theta2, int* yptr, float* J, float lambda, float* tempmemory){
+	int gid = blockIdx.x*blockDim.x + threadIdx.x;
+	if(gid<5000*10){
+		float ytemp=(float)(yptr[gid/10]==gid%10);
+		float ptemp=a3.ptr[gid];
+		//此处还可优化，因为如果让ytemp是int的话，在等于1时后面的log就可以不用算了 -ylxdzsw 2014.12.7
+		float costbc = (-1.0f*ytemp)*log(ptemp)-(1.0f-ytemp)*log(1-ptemp);
+		do {} while(atomicCAS(&lock,0,1)); // set lock
+		*J += costbc;
+		__threadfence(); // wait for write completion
+		lock = 0; // free lock
+	}
+	//这里也许不需要同步？
+	//__syncthreads();
+	Theta1.flag=1;Theta1.col--;
+	Theta2.flag=2;Theta2.col--;
+	int bid = gid-Theta1.row*Theta1.col;
+	if(bid < 0){
+		float t1temp = getitem(Theta1,gid/Theta1.col,gid%Theta1.col);
+		do {} while(atomicCAS(&lock,0,1)); // set lock
+		tempmemory[0] += t1temp*t1temp;
+		__threadfence(); // wait for write completion
+		lock = 0; // free lock
+	}
+	if(bid >= 0 && bid < Theta2.row*Theta2.col){
+		float t2temp = getitem(Theta2,bid/Theta2.col,bid%Theta2.col);
+		do {} while(atomicCAS(&lock,0,1)); // set lock
+		tempmemory[1] += t2temp*t2temp;
+		__threadfence(); // wait for write completion
+		lock = 0; // free lock
+	}
+	__syncthreads();
+	if(gid==0){
+		*J += (lambda/2.0f)*(tempmemory[0]+tempmemory[1]);
+		*J /= 5000.0f;
 	}
 }
 
@@ -144,8 +187,8 @@ int main(void) {
 
 	M  h_X, h_theta1, h_theta2,d_X, d_theta1,d_theta2,d_a3,d_a2;
 	h_X.ptr=new float[5000*400];h_X.row=5000;h_X.col=400;h_X.flag=0;
-	h_theta1.ptr=new float[401*25];h_theta1.row=401;h_theta1.col=25;h_theta1.flag=0;
-	h_theta2.ptr= new float[26*10];h_theta2.row=26;h_theta2.col=10;h_theta2.flag=0;
+	h_theta1.ptr=new float[25*401];h_theta1.row=25;h_theta1.col=401;h_theta1.flag=0;
+	h_theta2.ptr= new float[10*26];h_theta2.row=10;h_theta2.col=26;h_theta2.flag=0;
 	d_a3.ptr=0;d_a3.row=5000;d_a3.col=10;d_a3.flag=0;
 	d_a2.ptr=0;d_a2.row=5000;d_a2.col=26;d_a2.flag=-1;
 	int * h_yptr,* d_yptr;
@@ -155,8 +198,8 @@ int main(void) {
 	initdata<int>("Y.dat", 5000, h_yptr);
     initdata<float>("X2.dat", h_X.row*h_X.col, h_X.ptr);
 
-    inittheta(401*25,h_theta1.ptr);
-    inittheta(26*10,h_theta2.ptr);
+    inittheta(25*401,h_theta1.ptr);
+    inittheta(10*26,h_theta2.ptr);
     d_X=h_X;d_theta1=h_theta1;d_theta2=h_theta2;
     cudaSafeCall(cudaMalloc((void**) &d_X.ptr, sizeof(float) *d_X.col*d_X.row ));
     cudaSafeCall(cudaMalloc((void**) &d_yptr, sizeof(int) *5000 ));
@@ -170,11 +213,13 @@ int main(void) {
     cudaSafeCall(cudaMemcpy(d_theta1.ptr, h_theta1.ptr, sizeof(float)*d_theta1.col*d_theta1.row,cudaMemcpyHostToDevice));
     cudaSafeCall(cudaMemcpy(d_theta2.ptr, h_theta2.ptr, sizeof(float)*d_theta2.col*d_theta2.row,cudaMemcpyHostToDevice));
     cout<<"fuck";
-    nnCostFunction<<<5000*25/1024+1,1024>>>(d_theta1,d_theta2,d_X,d_yptr,0.1f,d_a2,d_a3,1);
+	for(int i=0;i<2;i++){
+		gradient<<<5000*25/1024+1,1024>>>(d_theta1,d_theta2,d_X,d_yptr,0.1f,d_a2,d_a3);
 
+	}
 
-//	CUDA_CHECK_RETURN(cudaThreadSynchronize());	// Wait for the GPU launched work to complete
-//	CUDA_CHECK_RETURN(cudaGetLastError());
+	cudaSafeCall(cudaThreadSynchronize());	// Wait for the GPU launched work to complete
+	cudaSafeCall(cudaGetLastError());
 //	CUDA_CHECK_RETURN(cudaMemcpy(odata, d, sizeof(int) * WORK_SIZE, cudaMemcpyDeviceToHost));
 //
 //	for (i = 0; i < WORK_SIZE; i++)
